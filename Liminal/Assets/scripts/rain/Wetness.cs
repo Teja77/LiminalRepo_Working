a9842f642aaae028c67e2f3d7fc7dfd8fde4a21d
Teja77 <saiteja9440@gmail.com>
2019-09-30 17:43:21 +1000

    Worked on tree leaves shader and started to work on snow accumulation shader

--- Liminal/Assets/scripts/rain/Wetness.cs
+++ Liminal/Assets/scripts/rain/Wetness.cs
@@ -8,12 +8,18 @@ public class Wetness : MonoBehaviour
     public float RainDelta;
     public float multiplier = 1f;
     public float MaxRainAmount;
+    public float MaxRippleAmount;
     private float _curRainVal;
+    private float _curRippleVal;
 
     private void OnParticleCollision(GameObject other)
     {
-        _curRainVal = Material.GetFloat("Vector1_A87DE66");
+        _curRainVal = Material.GetFloat("Vector1_FC03E501");
+        _curRippleVal = Material.GetFloat("Vector1_A87DE66");
+        _curRippleVal = Mathf.Clamp(_curRippleVal + (RainDelta * multiplier), 0f, MaxRippleAmount); ;
         _curRainVal = Mathf.Clamp(_curRainVal + (RainDelta * multiplier), 0f, MaxRainAmount);
-        Material.SetFloat("Vector1_A87DE66", _curRainVal);
+        Material.SetFloat("Vector1_FC03E501", _curRainVal);
+        Material.SetFloat("Vector1_A87DE66", _curRippleVal);
+
     }
 }
